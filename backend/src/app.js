032a7f2aa7e20b12198f09b152de945f5b0e0489
Kalana-sithum <kalanasithum1211@gmail.com>
2023-10-14 17:56:22 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import RateLimiterMemory from "rate-limiter-flexible/lib/RateLimiterMemory.js";
+import bodyParser from "body-parser";
 import cors from "cors";
 import logger from "./utils/logger";
 import helmet from "helmet";
@@ -10,6 +12,26 @@ import { connect } from "./utils/database.connection";
 const app = express();
 const PORT = process.env.PORT || "8090";
 
+const rateLimiter = new RateLimiterMemory({
+  points: 10, // maximum number of requests allowed
+  duration: 1, // time frame in seconds
+});
+
+// Register Rate Limiter Middleware
+app.use((req, res, next) => {
+  rateLimiter
+    .consume(req.ip)
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      res.status(429).send("Too Many Requests");
+    });
+});
+
+// set the request size limit to 1 MB
+app.use(bodyParser.json({ limit: '1mb' }));
+
 // Register Middleware Chain
 app.use(
   cors({
@@ -17,6 +39,18 @@ app.use(
     methods: "GET,PUT,POST,DELETE",
   })
 );
+// Add Content-Security-Policy header
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "frame-ancestors 'self'", "default-src 'none'", "script-src 'self'", "style-src 'self'", " img-src 'self'");
+  next();
+});
+
+// Add X-Frame-Options header
+app.use((req, res, next) => {
+  res.setHeader('X-Frame-Options', 'DENY');
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
